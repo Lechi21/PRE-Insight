fa49d6b08947c85d55e13fb49f7d195b112ca414
Kelechi Gift <lechi.scitech@gmail.com>
2024-01-06 22:27:50 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: '06.01.2024',
+        productNumber: '5700',
+        paymentStatus: '85700',
+        status: 'Pending'
+    },
+    {
+        productName: '#',
+        productNumber: '#',
+        paymentStatus: '#',
+        status: 'Declined'
+    },
+    {
+        productName: '#',
+        productNumber: '#',
+        paymentStatus: '#',
+        status: 'Active'
+    },
+]
